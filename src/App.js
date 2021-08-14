47ced92b8e05b24ee34fddbfd71bf5681cadcaad
rajadhikari1996 <tech.codetechyons>
2021-08-14 22:06:51 +0530

    remove slider component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import Header from './components/header'
 import ProductListing from './components/productListing'
 import ProductDetails from './components/productDetails'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Slider from './components/slider'
+import Example from './components/slider'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
+      <Example />
       {/* <h2>Hello</h2> */}
-      <Router>
+      {/* <Router>
         <Header />
         <Slider />
         <Switch>
@@ -17,7 +18,7 @@ function App() {
           <Route exact path="/product/:productId" component={ProductDetails} />
           <Route>404 Not Found</Route>
         </Switch>
-      </Router>
+      </Router> */}
     </div>
   )
 }
--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -1,55 +1,9 @@
-import React from 'react'
+import React from 'react';
 
-const Slider = () => {
-  return (
-    <>
-      <div
-        id="carouselExampleControls"
-        className="carousel slide"
-        data-bs-ride="carousel"
-      >
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img
-              src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"
-              className="d-block w-100"
-              alt="..."
-            />
-          </div>
-          <div className="carousel-item">
-            <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" className="d-block w-100" alt="..." />
-          </div>
-          <div className="carousel-item">
-            <img src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" className="d-block w-100" alt="..." />
-          </div>
-        </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleControls"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleControls"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-      </div>
-    </>
-  )
+const Example = () => {
+  return ( 
+    <div>Example</div>
+   );
 }
-
-export default Slider
+ 
+export default Example;
